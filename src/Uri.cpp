14c579c6564128c3c68f5cfd1d9cc309a6373476
eggveloper <94803635+eggveloper@users.noreply.github.com>
2021-11-26 21:28:42 +0200

    basic parsing done and all tests pass!

--- src/Uri.cpp
+++ src/Uri.cpp
@@ -9,6 +9,8 @@
  * © 2021 by Callum Ali
  */
 
+#include <string>
+#include <vector>
 #include <Uri/Uri.hpp>
 
 namespace Uri {
@@ -16,12 +18,95 @@ namespace Uri {
      * Contains the private properties of a Uri instance.
      */
     struct Uri::Impl {
+        /**
+         * @brief The character or character sequence
+         * that should be interpreted as a path delimiter.
+         */
+        std::string pathDelimiter = "/";
 
+        /**
+         * @brief The "scheme" element of the URI.
+         */
+        std::string scheme;
+
+        /**
+         * @brief The "host" element of the URI.
+         */
+        std::string host;
+
+        /**
+         * @brief The "path" element of the URI
+         * as a sequence of steps.
+         */
+        std::vector<std::string> path;
     };
 
     Uri::~Uri() = default;
 
     Uri::Uri() : impl_(new Impl) {}
+
+    void Uri::setPathDelimiter(const std::string& newPathDelimiter) {
+        impl_->pathDelimiter = newPathDelimiter;
+    }
+
+    bool Uri::parseFromString(const std::string& uriString) {
+        // Scheme Parsing //
+
+        const auto schemeEnd = uriString.find(':');
+        impl_->scheme = uriString.substr(0, schemeEnd);
+        auto rest = uriString.substr(schemeEnd + 1);
+
+        // Host Parsing //
+
+        if (rest.substr(0, 2) == "//") {
+            const auto authorityEnd = rest.find(impl_->pathDelimiter, 2);
+            impl_->host = rest.substr(2, authorityEnd - 2);
+            rest = rest.substr(authorityEnd);
+        } else {
+            impl_->host.clear();
+        }
+
+        // Path Parsing //
+
+        impl_->path.clear();
+
+        if (rest == impl_->pathDelimiter) {
+            // Special case of a path that is empty but needs a single
+            // empty-string element to indicate that it is absolute.
+
+            impl_->path.push_back("");
+        } else if (!rest.empty()) {
+            for(;;) {
+                auto pathDelimiter = rest.find(impl_->pathDelimiter);
+
+                if (pathDelimiter == std::string::npos) {
+                    impl_->path.push_back(rest);
+                    break;
+                } else {
+                    impl_->path.emplace_back(
+                        rest.begin(),
+                        rest.begin() + pathDelimiter
+                    );
+
+                    rest = rest.substr(pathDelimiter + impl_->pathDelimiter.length());
+                }
+            }
+        }
+
+        return true;
+    }
+
+    std::string Uri::getScheme() const {
+        return impl_->scheme;
+    }
+
+    std::string Uri::getHost() const {
+        return impl_->host;
+    }
+
+    std::vector<std::string> Uri::getPath() const {
+        return impl_->path;
+    }
 }
 
 #endif /* URI_HPP */
--- test/src/UriTests.cpp
+++ test/src/UriTests.cpp
@@ -7,9 +7,10 @@
  */
 
 #include <gtest/gtest.h>
+#include <stddef.h>
 #include <Uri/Uri.hpp>
 
-TEST(UriTests, ParseFromString) {
+TEST(UriTests, ParseFromStringUrl) {
     Uri::Uri uri;
 
     ASSERT_TRUE(uri.parseFromString("http://www.example.com/foo/bar"));
@@ -23,6 +24,26 @@ TEST(UriTests, ParseFromString) {
         }),
         uri.getPath()
     );
+}
+
+TEST(UriTests, ParseFromStringUrnDefaultPathDelimiter) {
+    Uri::Uri uri;
+
+    ASSERT_TRUE(uri.parseFromString("urn:book:fantasy:Hobbit"));
+    ASSERT_EQ("urn", uri.getScheme());
+    ASSERT_EQ("", uri.getHost());
+    ASSERT_EQ(
+        (std::vector<std::string> {
+            "book:fantasy:Hobbit",
+        }),
+        uri.getPath()
+    );
+}
+
+TEST(UriTests, ParseFromStringUrnSingleCharacterPathDelimiter) {
+    Uri::Uri uri;
+
+    uri.setPathDelimiter(":");
 
     ASSERT_TRUE(uri.parseFromString("urn:book:fantasy:Hobbit"));
     ASSERT_EQ("urn", uri.getScheme());
@@ -31,8 +52,51 @@ TEST(UriTests, ParseFromString) {
         (std::vector<std::string> {
             "book",
             "fantasy",
-            "hobbit",
+            "Hobbit",
         }),
         uri.getPath()
     );
+}
+
+TEST(UriTests, ParseFromStringUrnMultiCharacterPathDelimiter) {
+    Uri::Uri uri;
+
+    uri.setPathDelimiter("/-");
+
+    ASSERT_TRUE(uri.parseFromString("urn:bo-/o-k/-fant/asy/-Hob-bit"));
+    ASSERT_EQ("urn", uri.getScheme());
+    ASSERT_EQ("", uri.getHost());
+    ASSERT_EQ(
+        (std::vector<std::string> {
+            "bo-/o-k",
+            "fant/asy",
+            "Hob-bit",
+        }),
+        uri.getPath()
+    );
+}
+
+TEST(UriTests, ParseFromStringPathCornerCases) {
+    struct TestVector {
+        std::string pathIn;
+        std::vector<std::string> pathOut;
+    };
+
+    const std::vector<TestVector> testVectors {
+        {"", {}},
+        {"/", {""}},
+        {"/foo", {"", "foo"}},
+        {"foo/", {"foo", ""}},
+    };
+
+    size_t index = 0;
+
+    for (const auto& testVector : testVectors) {
+        Uri::Uri uri;
+
+        ASSERT_TRUE(uri.parseFromString(testVector.pathIn)) << "Tested vector " << index << ".";
+        ASSERT_EQ(testVector.pathOut, uri.getPath()) << "Tested vector " << index << ".";
+        
+        ++index;
+    }
 }
