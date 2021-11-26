5f5fd71b523bf78b1a742561fc42d91f24cabeaa
eggveloper <94803635+eggveloper@users.noreply.github.com>
2021-11-26 21:34:38 +0200

    include uri header..

--- include/Uri/Uri.hpp
+++ include/Uri/Uri.hpp
@@ -7,6 +7,8 @@
  */
 
 #include <memory>
+#include <string>
+#include <vector>
 
 namespace Uri {
     /**
@@ -25,6 +27,52 @@ namespace Uri {
          * Default Constructor
          */
         Uri();
+
+        /**
+         * @brief This method sets the character or character sequence
+         * that should be interpreted as a path delimiter.
+         * 
+         * @param[in] newPathDelimiter Character or character sequence
+         * that should be interpreted as a path delimiter.
+         */
+        void setPathDelimiter(const std::string& newPathDelimiter);
+
+        /**
+         * @brief This method builds the URI from the elements parsed
+         * from the given string rendering of a URI.
+         * 
+         * @param[in] uriString String rendering of a URI to parse.
+         * @return[bool] Indication of whether or not the URI was parsed successfully.
+         */
+        bool parseFromString(const std::string& uriString);
+
+        /**
+         * @brief This method returns the "scheme" element of the URI.
+         * 
+         * @return The "scheme" element of the URI.
+         * @retval "" is returned if there is no "scheme" element in the URI.
+         */
+        std::string getScheme() const;
+
+        /**
+         * @brief This method returns the "host" element of the URI.
+         * 
+         * @return The "host" element of the URI.
+         * @retval "" is returned if there is no "host" element in the URI.
+         */
+        std::string getHost() const;
+
+        /**
+         * @brief This method returns the "path" element of the URI
+         * as a sequence of steps.
+         * 
+         * @note If the first step of the path is an empty string,
+         * then the URI has an absolute path.
+         * 
+         * @return The "path" element of the URI as a sequence of steps.
+         * 
+         */
+        std::vector<std::string> getPath() const;
     private:
         struct Impl;
 
